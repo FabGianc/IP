4ea768c0691a4f7532ef9fb2b1180248dc2238c0
FabGianc <70634819+FabGianc@users.noreply.github.com>
2023-05-10 14:41:02 +0200

    Update find_ip.py

--- find_ip.py
+++ find_ip.py
@@ -1,7 +1,9 @@
+# Estrae gli indirizzi IP da un file di log e li memorizza in un file txt
+
 import re
 
 # Apri il file in modalità di lettura e leggi il contenuto
-with open('u_ex230329.log', 'r') as f:
+with open('file_di_log.log', 'r') as f:
     content = f.read()
 
 # Crea una regex per cercare gli indirizzi IP nel contenuto del file di testo
@@ -9,6 +11,6 @@ ip_regex = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
 ip_list = re.findall(ip_regex, content)
 
 # Apri un nuovo file in modalità di scrittura e scrivi gli indirizzi IP trovati
-with open('u_ex230329.txt', 'w') as output_file:
+with open('file_IP.txt', 'w') as output_file:
     for ip in ip_list:
         output_file.write(ip + '\n')
